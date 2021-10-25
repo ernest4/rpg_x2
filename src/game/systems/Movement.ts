c209ab5ef8ae0537fefc016f625a39129f4de72e
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-10-25 09:59:29 +0100

    movement system update logic as optimal as its gonna get. need to speed up query somehoow

--- src/game/systems/Movement.ts
+++ src/game/systems/Movement.ts
@@ -18,47 +18,19 @@ class Movement extends System {
   destroy(): void {}
 
   private updateTransforms = (querySet: QuerySet) => {
-    const [
-      {
-        position,
-        position: { x, y, z },
-        rotation,
-      },
-      {
-        stationary,
-        linearVelocity: { x: lvx, y: lvy, z: lvz },
-        angularVelocity: { z: avxyz },
-      },
-    ] = querySet as [Transform, PhysicsBody];
+    const [{ position, rotation }, { stationary, linearVelocity, angularVelocity }] = querySet as [
+      Transform,
+      PhysicsBody
+    ];
 
     const seconds = this.deltaTime / 1000;
 
-    // so far this is fast or faster?
-    // transform.position.x += physicsBody.linearVelocity.x * seconds;
-    // transform.position.y += physicsBody.linearVelocity.y * seconds;
-    // transform.position.z += physicsBody.linearVelocity.z * seconds;
+    position.x = position.x + linearVelocity.x * seconds;
+    position.y = position.y + linearVelocity.y * seconds;
+    position.z = position.z + linearVelocity.z * seconds;
 
-    // TODO: maybe try avoiding updates if no data change, see if improves...
-    // if (stationary) return;
-
-    // TODO: ALSO try NOT using array buffer, i.e. use plain object instead of Vector3BufferView.
-    // remember how buffers sucked vs plain array in the other benchmark. could be some V8
-    // optimization we're missing here too...!!!
-
-    // this is slightly faster?? like 0.5ms ?? worth it of course, but hard to tell...
-    // const { x, y, z } = position.xyz;
-    // const { x: lvx, y: lvy, z: lvz } = lvxyz;
-
-    const newX = x + lvx * seconds;
-    const newY = y + lvy * seconds;
-    const newZ = z + lvz * seconds;
-    // position = { x: newX, y: newY, z: newZ };
-    position.x = newX;
-    position.y = newY;
-    position.z = newZ;
-
-    rotation.z += avxyz * seconds;
-    if (360 < rotation.z) rotation.z = rotation.z - 360;
+    const newRotationZ = rotation.z + angularVelocity.z * seconds;
+    rotation.z = 360 < newRotationZ ? newRotationZ - 360 : newRotationZ;
   };
 }
 
